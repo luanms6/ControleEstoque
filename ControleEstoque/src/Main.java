140f3d2cd3d63b1b6d37aeb1adb081d7822290a8
Luan Peixoto <142679674+luanms6@users.noreply.github.com>
2024-03-12 21:28:33 -0300

    Add files via upload

---
+++ ControleEstoque/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println();
+        System.out.println("Product data: " + product.toString());
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product.toString());
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Update data: " + product.toString());
+
+        sc.close();
+    }
+}
